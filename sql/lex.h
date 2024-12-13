2039546e0af419954c318aac193a4bb6cb34f3af
Alessandro Vetere <alessandro.vetere@mariadb.com>
2024-12-13 14:50:34 +0000

    DEF0621798: PRB1843026: SAMU-274 - add DRY_RUN flag to ALTER TABLE
    Running an ALTER TABLE with the DRY_RUN flag will always make the query
    fail. This can happen because the server does not support the operation,
    meaning it would fail even without the DRY_RUN flag, or because the
    DRY_RUN flag itself blocks it. In such cases, a log will show where in
    the code the issue happened and what ALTER TABLE flags were involved. If
    the query would fail without the DRY_RUN flag, the error message is the
    same as if DRY_RUN was not used.
    
    The idea is that if this situation occurs, the user can retry the query
    without the DRY_RUN flag to see if it progresses further and possibly
    succeeds. This is especially useful for testing whether an ALTER TABLE
    ..., ALGORITHM=INSTANT query might work without fully executing it.
    
    Details about the new error codes are found in:
    - mysql-test/main/alter_table_dry_run.test
    
    DRY_RUN queries do NOT affect the binlog.
    
    The DRY_RUN flag uses the lexer flag "describe", which is typically used
    by EXPLAIN queries. In the future, a more general EXPLAIN ALTER TABLE ...
    syntax could replace ALTER TABLE ..., DRY_RUN.
    
    The error code ER_ALTER_DRY_RUN_ERROR is included to handle possible
    errors in INPLACE DRY_RUN operations. A DBUG_ASSERT(0) is used to catch
    such errors during debugging.
    
    The DRY_RUN flag seems to be NOT supported with the ALTER TABLE PARTITION
    syntax and was NOT tested in this context.

--- sql/lex.h
+++ sql/lex.h
@@ -200,6 +200,8 @@ SYMBOL symbols[] = {
   { "DOUBLE",		SYM(DOUBLE_SYM)},
   { "DO_DOMAIN_IDS",    SYM(DO_DOMAIN_IDS_SYM)},
   { "DROP",		SYM(DROP)},
+  { "DRY",		SYM(DRY_SYM)},
+  { "DRY_RUN",		SYM(DRY_RUN_SYM)},
   { "DUAL",		SYM(DUAL_SYM)},
   { "DUMPFILE",		SYM(DUMPFILE)},
   { "DUPLICATE",	SYM(DUPLICATE_SYM)},
