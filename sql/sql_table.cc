119b25c7ba3c95f6b87a02392bb72cc2f82836e8
Yury Chaikou <yury.chaikou@servicenow.com>
2025-01-15 17:30:26 -0800

    STRY56031688: Safe replication of ALTER TABLE ddl
    Problem
    =======
    The master executes ALTER TABLE ADD KEY on a temporary table and then drops
    it. The ADD KEY operation is scheduled for execution in a separate domain
    (domain 7), while the DROP TABLE goes to the default domain (domain 0).
    
    Because domain 7 might be busy (for example, executing another ADD KEY), it
    is possible that the DROP TABLE begins before the ADD KEY. As a result, the
    slave raises an error - "table does not exist" and stops replication.
    
    Workaround
    ==========
    There is a global variable, slave_ddl_exec_mode, which can be set to STRICT
    or IDEMPOTENT (default). In IDEMPOTENT mode, the replica does not stop for
    failed DDL operations that would not cause a difference between the primary
    and the replica. For example, CREATE TABLE is treated as CREATE OR REPLACE
    TABLE, and DROP TABLE is treated as DROP TABLE IF EXISTS.
    
    This change extends the same behavior to ALTER TABLE.
    When slave_ddl_exec_mode = IDEMPOTENT, all ALTER TABLE statements on the slave
    are treated as ALTER TABLE IF EXISTS.

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -10837,9 +10837,18 @@ bool mysql_alter_table(THD *thd, const LEX_CSTRING *new_db,
 
   thd->open_options|= HA_OPEN_FOR_ALTER;
   thd->mdl_backup_ticket= 0;
+
+  if (thd->slave_thread &&
+      slave_ddl_exec_mode_options == SLAVE_EXEC_MODE_IDEMPOTENT)
+  {
+    table_list->open_strategy= TABLE_LIST::OPEN_IF_EXISTS;
+  }
+
   error= open_tables(thd, &table_list, &tables_opened, 0,
                      &alter_prelocking_strategy);
   thd->open_options&= ~HA_OPEN_FOR_ALTER;
+  table= table_list->table;
+  error|= table == NULL;
 
   if (unlikely(error))
   {
@@ -10861,7 +10870,6 @@ bool mysql_alter_table(THD *thd, const LEX_CSTRING *new_db,
     DBUG_RETURN(true);
   }
 
-  table= table_list->table;
   bool is_reg_table= table->s->tmp_table == NO_TMP_TABLE;
   
 #ifdef WITH_WSREP
