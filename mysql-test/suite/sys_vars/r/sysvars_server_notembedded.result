knielsen/server

commit 2e814d4702d71a04388386a9f591d14a35980bfe

Merge InnoDB 5.7 from mysql-5.7.9.
Contains also

MDEV-10547: Test multi_update_innodb fails with InnoDB 5.7

	The failure happened because 5.7 has changed the signature of
	the bool handler::primary_key_is_clustered() const
	virtual function ("const" was added). InnoDB was using the old
	signature which caused the function not to be used.

MDEV-10550: Parallel replication lock waits/deadlock handling does not work with InnoDB 5.7

	Fixed mutexing problem on lock_trx_handle_wait. Note that
	rpl_parallel and rpl_optimistic_parallel tests still
	fail.

MDEV-10156 : Group commit tests fail on 10.2 InnoDB (branch bb-10.2-jan)
  Reason: incorrect merge

MDEV-10550: Parallel replication can't sync with master in InnoDB 5.7 (branch bb-10.2-jan)
  Reason: incorrect merge

mysql-test/r/mysqld--help.result
@@ -108,9 +108,6 @@ The following options may be given as the first argument:
  --bulk-insert-buffer-size=# 
  Size of tree cache used in bulk insert optimisation. Note
  that this is a limit per thread!
- --changed-page-bitmaps[=name] 
- Enable or disable CHANGED_PAGE_BITMAPS plugin. One of:
- ON, OFF, FORCE (don't start if the plugin fails to load).
  --character-set-client-handshake 
  Don't ignore client side character set value sent during
  handshake.
@@ -1153,7 +1150,6 @@ binlog-row-event-max-size 1024
 binlog-row-image FULL
 binlog-stmt-cache-size 32768
 bulk-insert-buffer-size 8388608
-changed-page-bitmaps ON
 character-set-client-handshake TRUE
 character-set-filesystem binary
 character-sets-dir MYSQL_CHARSETSDIR/

commit c98d8bd9cee10b9010cc937fc86326eebe4d03a1

DEF0486394: PRB1733771: MDEV-29883 - Deadlock between InnoDB statistics update and BLOB insert
This is a backport of commit 8b6a308e463f937eb8d2498b04967a222c83af90
from MariaDB Server 10.6.11. No attempt to reproduce the hang
in earlier an earlier version of MariaDB Server than 10.6 was made.

In each caller of fseg_n_reserved_pages() except ibuf_init_at_db_start()
which is a special case for ibuf.index at database startup, we must hold
an index latch that prevents concurrent allocation or freeing of index
pages.

Any operation that allocates or free pages that belong to an index tree
must first acquire an index latch in non-shared mode, and while
holding that, acquire an index root page latch in non-shared mode.

btr_get_size(), btr_get_size_and_reserved(): Assert that a strong enough
index latch is being held.

dict_stats_update_transient_for_index(),
dict_stats_analyze_index(): Acquire a strong enough index latch.

These operations had followed the same order of acquiring latches in
every InnoDB version since the very beginning
(commit c533308a158795f91247e9fe3c7304fa5e7d2b3c).

The hang was introduced in
commit 2e814d4702d71a04388386a9f591d14a35980bfe which imported
mysql/mysql-server@ac74632293bea967b352d1b472abedeeaa921b98
which failed to strengthen the locking requirements of the function
btr_get_size().

sql/mysqld.cc
@@ -469,6 +469,7 @@ ulong snc_default_lock_expiration;
 ulonglong snc_lock_memory_target;
 uint32 snc_master_ddl_repl_subdomain_id;
 uint32 snc_slave_ddl_repl_subdomain_id;
+my_bool snc_enable_mdev29833;
 ulong open_files_limit, max_binlog_size;
 ulong slave_trans_retries;
 ulong slave_trans_retry_interval;

sql/mysqld.h
@@ -236,6 +236,7 @@ extern ulong snc_default_lock_expiration;
 extern ulonglong snc_lock_memory_target;
 extern uint32 snc_master_ddl_repl_subdomain_id;
 extern uint32 snc_slave_ddl_repl_subdomain_id;
+extern my_bool snc_enable_mdev29833;
 extern MYSQL_PLUGIN_IMPORT ulong max_connections;
 extern uint max_digest_length;
 extern ulong max_connect_errors, connect_timeout;

sql/sys_vars.cc
@@ -3518,6 +3518,12 @@ static Sys_var_ulonglong Sys_snc_lock_memory_target(
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
        ON_UPDATE(fix_snc_lock_memory_target));
 
+static Sys_var_mybool Sys_snc_enable_mdev29833(
+       "snc_enable_mdev29833",
+       "Enable fix for MDEV-29833:Deadlock between InnoDB statistics update and BLOB insert",
+       PARSED_EARLY READ_ONLY GLOBAL_VAR(snc_enable_mdev29833),
+       CMD_LINE(OPT_ARG), DEFAULT(FALSE),
+       NO_MUTEX_GUARD, NOT_IN_BINLOG);
 
 export sql_mode_t expand_sql_mode(sql_mode_t sql_mode)
 {

storage/innobase/btr/btr0btr.cc
@@ -614,7 +614,7 @@ btr_get_size(
 
 	ut_ad(srv_read_only_mode
 	      || mtr_memo_contains(mtr, dict_index_get_lock(index),
-				   MTR_MEMO_S_LOCK));
+				snc_enable_mdev29833 ? MTR_MEMO_SX_LOCK : MTR_MEMO_S_LOCK));
 
 	if (index->page == FIL_NULL
 	    || dict_index_is_online_ddl(index)
@@ -668,7 +668,7 @@ btr_get_size_and_reserved(
 	ulint		dummy;
 
 	ut_ad(mtr_memo_contains(mtr, dict_index_get_lock(index),
-				MTR_MEMO_S_LOCK));
+				snc_enable_mdev29833 ? MTR_MEMO_SX_LOCK : MTR_MEMO_S_LOCK));
 
 	ut_a(flag == BTR_N_LEAF_PAGES || flag == BTR_TOTAL_SIZE);
 

storage/innobase/dict/dict0defrag_bg.cc
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 2016, 2019, MariaDB Corporation.
+Copyright (c) 2016, 2022, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -45,6 +45,7 @@ by background defragmentation. */
 defrag_pool_t			defrag_pool;
 
 extern bool dict_stats_start_shutdown;
+extern my_bool snc_enable_mdev29833;
 
 /*****************************************************************//**
 Initialize the defrag pool, called once during thread initialization. */
@@ -280,7 +281,16 @@ dict_stats_save_defrag_stats(
 	ulint	n_leaf_pages;
 	ulint	n_leaf_reserved;
 	mtr.start();
-	mtr_s_lock_index(index, &mtr);
+
+	if (snc_enable_mdev29833)
+	{
+		mtr_sx_lock_index(index, &mtr);
+	}
+	else
+	{
+		mtr_s_lock_index(index, &mtr);
+	}
+
 	n_leaf_reserved = btr_get_size_and_reserved(index, BTR_N_LEAF_PAGES,
 						    &n_leaf_pages, &mtr);
 	mtr.commit();

storage/innobase/dict/dict0stats.cc
@@ -1,7 +1,7 @@
 /*****************************************************************************
 
 Copyright (c) 2009, 2019, Oracle and/or its affiliates. All Rights Reserved.
-Copyright (c) 2015, 2021, MariaDB Corporation.
+Copyright (c) 2015, 2022, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -851,7 +851,16 @@ dict_stats_update_transient_for_index(
 		ulint	size;
 
 		mtr.start();
-		mtr_s_lock_index(index, &mtr);
+
+		if (snc_enable_mdev29833)
+		{
+			mtr_sx_lock_index(index, &mtr);
+		}
+		else
+		{
+			mtr_s_lock_index(index, &mtr);
+		}
+
 		size = btr_get_size(index, BTR_TOTAL_SIZE, &mtr);
 
 		if (size != ULINT_UNDEFINED) {
@@ -1944,7 +1953,16 @@ static index_stats_t dict_stats_analyze_index(dict_index_t* index)
 	DEBUG_PRINTF("  %s(index=%s)\n", __func__, index->name());
 
 	mtr.start();
-	mtr_s_lock_index(index, &mtr);
+
+	if (snc_enable_mdev29833)
+	{
+		mtr_sx_lock_index(index, &mtr);
+	}
+	else
+	{
+		mtr_s_lock_index(index, &mtr);
+	}
+
 	size = btr_get_size(index, BTR_TOTAL_SIZE, &mtr);
 
 	if (size != ULINT_UNDEFINED) {

mysql-test/main/mysqld--help.result
@@ -1279,6 +1279,9 @@ The following specify which files/extra groups are read (specified before remain
  --snc-default-lock-expiration=# 
  Default expiration time a session can hold a ServiceNow
  lock for (seconds)
+ --snc-enable-mdev29833 
+ Enable fix for MDEV-29833:Deadlock between InnoDB
+ statistics update and BLOB insert
  --snc-lock-memory-target=# 
  Target amount of memory for the ServiceNow lock memory
  pool.
@@ -1791,6 +1794,7 @@ slow-query-log FALSE
 sn-order-by-limit-optimize-level 2
 sn-order-by-limit-row-threshold 300000
 snc-default-lock-expiration 120
+snc-enable-mdev29833 FALSE
 snc-lock-memory-target 1073741824
 snc-master-ddl-repl-subdomain-id 0
 snc-slave-ddl-repl-subdomain-id 0

mysql-test/main/sn_order_by_level_0.result
@@ -18,6 +18,7 @@ Variable_name	Value
 sn_order_by_limit_optimize_level	2
 sn_order_by_limit_row_threshold	300000
 snc_default_lock_expiration	120
+snc_enable_mdev29833	OFF
 snc_lock_memory_target	1073741824
 snc_master_ddl_repl_subdomain_id	0
 snc_slave_ddl_repl_subdomain_id	0

mysql-test/suite/sys_vars/r/sysvars_server_notembedded.result
@@ -3762,6 +3762,16 @@ NUMERIC_BLOCK_SIZE	1
 ENUM_VALUE_LIST	NULL
 READ_ONLY	NO
 COMMAND_LINE_ARGUMENT	REQUIRED
+VARIABLE_NAME	SNC_ENABLE_MDEV29833
+VARIABLE_SCOPE	GLOBAL
+VARIABLE_TYPE	BOOLEAN
+VARIABLE_COMMENT	Enable fix for MDEV-29833:Deadlock between InnoDB statistics update and BLOB insert
+NUMERIC_MIN_VALUE	NULL
+NUMERIC_MAX_VALUE	NULL
+NUMERIC_BLOCK_SIZE	NULL
+ENUM_VALUE_LIST	OFF,ON
+READ_ONLY	YES
+COMMAND_LINE_ARGUMENT	OPTIONAL
 VARIABLE_NAME	SNC_LOCK_MEMORY_TARGET
 VARIABLE_SCOPE	GLOBAL
 VARIABLE_TYPE	BIGINT UNSIGNED