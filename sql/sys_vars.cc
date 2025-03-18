eb8f262ea538eb977ba29dd794692e6c530bb9d0
Yury Chaikou <yury.chaikou@servicenow.com>
2025-03-18 12:00:38 -0700

    DEF0596622: PRB1822465: MDEV-36137 - Parallel replication deadlock between DML in one domain and DDL in another
    A deadlock could occur with out-of-order parallel replication, when a DDL in
    domain B runs in parallel with conflicting DML in domain A. The problem
    occurs when one transaction T2 in domain A is blocking the DDL, but is
    itself waiting on an earlier transaction T1, and T1 is in turn blocked by
    the DDL. This happens because a DDL waiting on an exclusive table lock will
    block new transactions from getting even a shared lock.
    
    This patch has a test case that demonstrates this. And a fix for this
    specific case, which detects that the DDL is blocked by a later transaction
    T2 and deadlock kills T2 (so it will roll back and retry after the DDL
    completes).
    
    Signed-off-by: Kristian Nielsen <knielsen@knielsen-hq.org>

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -2089,7 +2089,7 @@ static Sys_var_uint Sys_snc_slave_max_synchronized_domain_id(
        "Synchronize transactions in domain N with those in domain 0 "
        "when N <= snc_slave_max_synchronized_domain_id",
        GLOBAL_VAR(snc_slave_max_synchronized_domain_id),
-       CMD_LINE(REQUIRED_ARG), VALID_RANGE(0, UINT_MAX32), DEFAULT(100),
+       CMD_LINE(REQUIRED_ARG), VALID_RANGE(0, UINT_MAX32), DEFAULT(64),
        BLOCK_SIZE(1),
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0));
 
